acce4490e692bca4e90477747ce79ed2b84661a3
KevinLu <78304455+KevinLu2000@users.noreply.github.com>
2024-01-29 00:17:24 -0500

    update nvm & comp legacy results

--- legacy_configs/script.sh
+++ legacy_configs/script.sh
@@ -6,7 +6,7 @@ a_max=10
 
 while [ $a -le $a_max ]
 do
-	./fio --name=randread1 --filename=testfile --ioengine=libaio --iodepth=1 --rw=randread --bs=4k --direct=1 --numjobs=$a --runtime=300 --group_reporting >> out_r4.txt
+	fio --name=randread1 --filename=testfile --ioengine=libaio --iodepth=1 --rw=randread --bs=4k --direct=1 --numjobs=$a --runtime=300 --group_reporting >> out_r4.txt
    	a=`expr $a + 1`
 done
 
@@ -17,7 +17,7 @@ a_max=10
 
 while [ $a -le $a_max ]
 do
-	./fio --name=randread2 --filename=testfile --ioengine=libaio --iodepth=1 --rw=randread --bs=8k --direct=1 --numjobs=$a --runtime=300 --group_reporting >> out_r8.txt
+	fio --name=randread2 --filename=testfile --ioengine=libaio --iodepth=1 --rw=randread --bs=8k --direct=1 --numjobs=$a --runtime=300 --group_reporting >> out_r8.txt
    	a=`expr $a + 1`
 done
 
@@ -28,7 +28,7 @@ a_max=10
 
 while [ $a -le $a_max ]
 do
-	./fio --name=randwrite1 --filename=testfile --ioengine=libaio --iodepth=1 --rw=randwrite --bs=4k --direct=1 --numjobs=$a --runtime=300 --group_reporting >> out_w4.txt
+	fio --name=randwrite1 --filename=testfile --ioengine=libaio --iodepth=1 --rw=randwrite --bs=4k --direct=1 --numjobs=$a --runtime=300 --group_reporting >> out_w4.txt
    	a=`expr $a + 1`
 done
 
@@ -39,6 +39,6 @@ a_max=10
 
 while [ $a -le $a_max ]
 do
-	./fio --name=randwrite2 --filename=testfile --ioengine=libaio --iodepth=1 --rw=randwrite --bs=4k --direct=1 --numjobs=$a --runtime=300 --group_reporting >> out_w8.txt
+	fio --name=randwrite2 --filename=testfile --ioengine=libaio --iodepth=1 --rw=randwrite --bs=4k --direct=1 --numjobs=$a --runtime=300 --group_reporting >> out_w8.txt
    	a=`expr $a + 1`
 done
